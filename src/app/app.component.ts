bf011b6658868997166c53878d4d525e9856ca80
Pablo Moraga <pablo.moraga.aedo@gmail.com>
2017-09-09 11:36:18 -0300

    Funciona

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,24 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+
+declare var $:any;
+declare var jQuery:any;
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   title = 'app';
+
+  ngOnInit() {
+    $('[data-toggle="popover"]').popover(); 
+  }
+
+  /**
+   * toogleButton
+   */
+  public toggleButton() {
+    $('#button01').button('toggle');
+  }
 }
